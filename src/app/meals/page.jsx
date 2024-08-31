944c98095f3b88fc5939913b16a2235df3129b6e
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-08-31 20:49:30 +0600

    Add some

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,16 +1,31 @@
-import Meals from '@/Components/Meals';
-import React from 'react';
+import Meals from "@/Components/Meals";
+import { Roboto } from "next/font/google";
+import Image from "next/image";
 
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time", { cache: "no-store" });
+  const data = await res.json();
+  console.log(data);
 
+  return data.currentTime;
+};
 
-const MealPage = () => {
-    return (
-        <div>
-            <h2 className='text-2xl text-red-400 font-bold'>Our meal is hotter than your gf</h2>
-            <h4>Choose your meal: </h4>
-            <Meals/>
-        </div>
-    );
+const roboto = Roboto({ weight: ["100", "300"], subsets: ["latin"] });
+
+const MealPage = async () => {
+  const realTime = await getTime();
+  return (
+    <div>
+      <h2 className={`${roboto.className} text-2xl text-red-400 font-bold`}>
+        Our meal is hotter than your gf
+      </h2>
+      <h4>Choose your meal: </h4>
+      <Meals />
+
+      <Image src="/1.jpg" alt="" height={1000} width={900} />
+      <h3>Time: {realTime}</h3>
+    </div>
+  );
 };
 
 export default MealPage;
---
+++ src/app/posts/[id]/loading.jsx
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <h2>This is loading spinner..............</h2>;
+};
+
+export default loading;
